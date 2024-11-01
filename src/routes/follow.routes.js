5b40da8abb961d2c4fc3a6a5af61e2154335cc4c
meeyth <anurupbhowmick2003@gmail.com>
2024-11-01 17:34:47 +0530

    implement get follower,following-con

--- src/routes/follow.routes.js
+++ src/routes/follow.routes.js
@@ -15,7 +15,7 @@ const router = Router()
 router.route("/toggle-follow/:userId").put(verifyJwt, toggleFollow)
 router.route("/get-following/:userId").get(verifyJwt, getFollowing)
 router.route("/get-follower/:userId").get(verifyJwt, getFollower)
-router.route("/get-follower/:userId").get(verifyJwt, getFollower)
+// router.route("/get-follower/:userId").get(verifyJwt, getFollower)
 // router.route("/is/:userId").get(verifyJwt, getFollower)
 
 export default router
