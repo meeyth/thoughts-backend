870406379df4327aa47d1ab7d814a157c5ea98cc
meeyth <anurupbhowmick2003@gmail.com>
2025-06-19 13:32:05 +0530

    update multer middleware to add public/temp dir in prod env

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,15 +1,21 @@
-import multer from "multer"
+import multer from "multer";
+import fs from "fs";
+import path from "path";
 
-const storage = multer.diskStorage(
-    {
-        destination: function (req, file, cb) {
-            cb(null, './public/temp')
-        },
-        filename: function (req, file, cb) {
-            // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-            cb(null, file.originalname)
-            console.log(file.originalname);
-        }
-    }
-)
-export const upload = multer({ storage, })
+const TEMP_DIR = path.join(__dirname, "..", "public", "temp");
+
+// Create the folder if it doesn't exist
+if (!fs.existsSync(TEMP_DIR)) {
+    fs.mkdirSync(TEMP_DIR, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, TEMP_DIR);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+
+export const upload = multer({ storage });
