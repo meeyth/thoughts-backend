4abba5842add1c0c717ce5e5c59e55e0351f8efc
meeyth <anurupbhowmick2003@gmail.com>
2025-06-23 18:06:36 +0530

    send user details while refreshing token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -206,7 +206,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
             .json(
                 new ApiResponse(
                     200,
-                    { accessToken, refreshToken },
+                    { user, accessToken, refreshToken },
                     "Access Token refreshed"
                 )
             )
